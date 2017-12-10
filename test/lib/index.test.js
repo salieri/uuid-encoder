ed52ad618fa9f92a2be0dbaa27de7bb525f262e8
Aleksi Asikainen <aleksi.asikainen@hpi.co.uk>
2017-12-10 17:53:32 -0600

    More init

--- test/lib/index.test.js
+++ test/lib/index.test.js
@@ -20,6 +20,7 @@ describe(
               (uuid) => {
                 const encoded = encoder.encode(uuid);
 
+                encoded.length.should.be.at.least(1);
                 encoded.should.not.equal(uuid);
 
                 const decoded = encoder.decode(encoded);
@@ -27,6 +28,32 @@ describe(
                 decoded.should.equal(uuid);
               },
             );
+          },
+        );
+      },
+    );
+
+
+    it(
+      'should support custom encoding sets',
+      () => {
+        const encoding  = 'ABCDEFG';
+        const encoder   = new UuidEncoder(encoding);
+
+        _.each(
+          TestData.uuids,
+          (uuid) => {
+            const encoded = encoder.encode(uuid);
+
+            encoded.should.not.equal(uuid);
+
+            encoded.length.should.be.at.least(1);
+            encoded.should.match(/[ABCDEFG]/);
+            encoded.should.not.match(/[^ABCDEFG]/);
+
+            const decoded = encoder.decode(encoded);
+
+            decoded.should.equal(uuid);
           },
         );
       },
